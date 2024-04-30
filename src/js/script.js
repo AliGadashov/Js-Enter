1d0140030ded5f951be3a0b70cdf18a5103e67ff
Ali Gadashov <gadashovali13@gmail.com>
2024-04-30 10:10:07 +0400

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+var firstName = prompt("Please enter your name: ");
+var surname = prompt("Please enter your surname: ");
+var birthdayYear = prompt("Please enter your birthday year: ");
+
+var now = 2024;
+var age = now - birthdayYear;
+
+console.log(firstName + " " + surname + " You are " + age + " years old");
+alert(firstName + " " + surname + " You are " + age + " years old")
